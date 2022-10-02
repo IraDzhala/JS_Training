560ac17cc7e35361de65a6288d913c7758644bde
IraDzhala <djala91y@gmail.com>
2022-10-02 18:25:17 +0300

    homework_operators

---
+++ operators.js
@@ -0,0 +1,91 @@
+//Example 1//
+let x = 7;
+let y = 3;
+x += y;
+console.log(x);
+//Example 2//
+let a = 10;
+let b = 5;
+a -= b;
+console.log(a);
+//Example 3//
+let x1 = 2;
+let y2 = 4;
+let z = 5;
+x1 *= y2;
+x1 *= z;
+console.log(x1);
+//Example 4//
+let a1 = 9;
+let b1 = 3;
+a1 /= b1;
+console.log(a1);
+//Example 5//
+let x3 = 45;
+let y3 = 7;
+x3 %= y3;
+console.log(x3);
+//Example 6//
+let c = 5;
+let d = 4;
+c **= d;
+console.log(c);
+//Example 7//
+let r = 3;
+let k = '3';
+let result = r == k;
+console.log(result);
+//Example 8//
+let r1 = 10;
+let k1 = '10';
+let result1 = r1 === k1;
+console.log(result1);
+//Example 9//
+let f = 21;
+let g = '21';
+let result2 = f != g;
+console.log(result2);
+//Example 10//
+let f1 = 9;
+let g1 = '9';
+let result3 = f1 !== g1;
+console.log(result3);
+//Example 11//
+let h = 5;
+let j = 6;
+let result4 = h > j;
+console.log(result4);
+//Example 12//
+let h1 = 6;
+let j1 = 7;
+let result5 = h1 < j1;
+console.log(result5);
+//Example 13//
+let v = 13;
+let n = 7;
+let result6 = v >= n;
+console.log(result6);
+//Example 14//
+let v1 = 12;
+let n1 = 8;
+let result7 = v1 <= n1;
+console.log(result7);
+//Example 15//
+let age = 21;
+let result8 = age >= 21 ? "age more than 21" : "age less than 21";
+console.log(result8);
+//Example 16//
+let l = 5;
+let t = 7;
+let result9 = (l < 10 && t > 8);
+console.log(result9);
+//Example 17//
+let p = 8;
+let e = 6;
+let result10 = (x == 8 || y == 0);
+console.log(result10);
+//Example 18//
+let i = 5;
+let w = 1;
+let result11 = !(i > w);
+console.log(result11);
